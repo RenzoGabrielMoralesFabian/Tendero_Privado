ab2781a6d9d9133212b10f767c986a37259f1021
73769972@continental.edu.pe <73769972@continental.edu.pe>
2022-10-24 13:36:03 -0500

    Nuevo

--- app/src/main/java/mx/com/encargalo/tendero/Inicio_sesion/ui/Mi_Aprendizaje/ap_frgmenuaprendizaje.java
+++ app/src/main/java/mx/com/encargalo/tendero/Inicio_sesion/ui/Mi_Aprendizaje/ap_frgmenuaprendizaje.java
@@ -1,21 +1,18 @@
 package mx.com.encargalo.tendero.Inicio_sesion.ui.Mi_Aprendizaje;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import mx.com.encargalo.R;
 
 public class ap_frgmenuaprendizaje extends Fragment {
 
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View vista = inflater.inflate(R.layout.fragment_ap_frgmenuaprendizaje, container, false);
 
-        return inflater.inflate(R.layout.fragment_ap_frgmenuaprendizaje, container, false);
+        return vista;
     }
 }
--- app/src/main/java/mx/com/encargalo/tendero/Inicio_sesion/ui/Mi_publicidad/pub_frgmenupublicidad.java
+++ app/src/main/java/mx/com/encargalo/tendero/Inicio_sesion/ui/Mi_publicidad/pub_frgmenupublicidad.java
@@ -3,20 +3,24 @@ package mx.com.encargalo.tendero.Inicio_sesion.ui.Mi_publicidad;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import mx.com.encargalo.R;
 
 public class pub_frgmenupublicidad extends Fragment {
 
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_pub_frgmenupublicidad, container, false);
+        View vista = inflater.inflate(R.layout.fragment_pub_frgmenupublicidad, container, false);
 
+        return vista;
     }
 }
