5fb93e72509766ba1ff46e8494030871c5c8e36a
73769972@continental.edu.pe <73769972@continental.edu.pe>
2022-11-02 23:34:11 -0500

    Ultimo mejorado

---
+++ app/src/main/java/mx/com/encargalo/tendero/UTIL/DATOS.java
@@ -0,0 +1,17 @@
+package mx.com.encargalo.tendero.UTIL;
+
+public class DATOS {
+
+    //public static final String IP_SERVER = "";
+    public static final String IP_SERVER = "http://129.151.103.228/Encargalo/APIS/TenderoApp/";
+
+    //SHAREDPREFERENCES para herencia de datos
+    public static final String SHAREDPREFERENCES = "SHAREDPREFERENCES_DATOS";
+
+    //Variables heredadas
+    public static final String NO_ENCONTRADO = "NO ENCONTRADO";
+
+    //MENU
+    public static final String VARGOB_ID_TENDERO = "id_tendero";
+
+}
