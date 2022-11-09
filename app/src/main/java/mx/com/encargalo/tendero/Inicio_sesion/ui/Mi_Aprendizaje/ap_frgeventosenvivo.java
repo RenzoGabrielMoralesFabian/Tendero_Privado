8e638c1ddf9a4daba8d513115eb2c905833af4af
fernandobullonarango <82955102+fernandobullonarango@users.noreply.github.com>
2022-11-08 22:29:08 -0500

    conexion con la BD

--- app/src/main/java/mx/com/encargalo/tendero/Inicio_sesion/ui/Mi_Aprendizaje/ap_frgeventosenvivo.java
+++ app/src/main/java/mx/com/encargalo/tendero/Inicio_sesion/ui/Mi_Aprendizaje/ap_frgeventosenvivo.java
@@ -8,6 +8,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import mx.com.encargalo.R;
 import mx.com.encargalo.tendero.Inicio_sesion.ui.Mi_Aprendizaje.Adapters.ap_adplistadofrgeventosenvivo;
@@ -19,20 +30,66 @@ public class ap_frgeventosenvivo extends Fragment {
     RecyclerView ap_rclvevento;
     ArrayList<EventoEnVivo> ap_listaeventoenvivo;
 
+    EventoEnVivo eventoEnVivo;
+    RequestQueue requestQueue;
+    JsonObjectRequest request;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View vista = inflater.inflate(R.layout.fragment_ap_frgeventosenvivo, container, false);
         ap_rclvevento =vista.findViewById(R.id.ap_eevrclveventos);
         ap_listaeventoenvivo = new ArrayList<>();
 
-        ap_cargaLista();
+        requestQueue = Volley.newRequestQueue(getContext());
 
-        ap_mostrarData();
+        parseJSON();
+
+//        ap_cargaLista();
+//
+//        ap_mostrarData();
 
         return vista;
     }
 
-    private void ap_cargaLista() {
+    private void parseJSON() {
+
+        String URL = "http://129.151.103.228/Encargalo/APIS/TenderoApp/c_consultar_eventos_aprendizaje.php";
+
+        request = new JsonObjectRequest(Request.Method.GET, URL, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        eventoEnVivo = null;
+                        JSONArray json=response.optJSONArray("eventos"); ///--------------------///--------------------///--------------------
+                        try {
+                            ap_listaeventoenvivo.clear();
+                            for (int i = 0; i<json.length();i++){
+                                eventoEnVivo=new EventoEnVivo();
+                                JSONObject jsonObject=null;
+                                jsonObject=json.getJSONObject(i);
+                                eventoEnVivo.setAp_strcharla(jsonObject.optString("evTitulo"));
+                                eventoEnVivo.setAp_strfecha(jsonObject.optString("evEstado"));
+                                ap_listaeventoenvivo.add(eventoEnVivo);
+
+                            }
+                            ap_adpeventosenvivo = new ap_adplistadofrgeventosenvivo(getContext(), ap_listaeventoenvivo);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getContext(), "Error al consultar"+ error.toString(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        requestQueue.add(request);
+
+    }
+
+    /*private void ap_cargaLista() {
         ap_listaeventoenvivo.add(new EventoEnVivo("Conoce sobre el programa Ventas top","5 sept 2022 07:00 p.m. Lima"));
         ap_listaeventoenvivo.add(new EventoEnVivo("Conoce sobre el programa Ventas top","5 sept 2022 07:00 p.m. Lima"));
     }
@@ -49,5 +106,5 @@ public class ap_frgeventosenvivo extends Fragment {
                 Toast.makeText(getContext(), "Seleccionó: " + charla, Toast.LENGTH_SHORT).show();
             }
         });
-    }
+    }*/
 }
--- app/src/main/java/mx/com/encargalo/tendero/Inicio_sesion/ui/Mi_publicidad/pub_frglistadomisanuncios.java
+++ app/src/main/java/mx/com/encargalo/tendero/Inicio_sesion/ui/Mi_publicidad/pub_frglistadomisanuncios.java
@@ -9,6 +9,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import mx.com.encargalo.R;
 import mx.com.encargalo.tendero.Inicio_sesion.ui.Mi_publicidad.Adapters.pub_adplistadofrglistado;
@@ -20,12 +31,20 @@ public class pub_frglistadomisanuncios extends Fragment {
     RecyclerView pub_rclvpublicidad;
     ArrayList<Publicidad> pub_listapublicidad;
 
+    /*Publicidad publicidad;
+    RequestQueue requestQueue;
+    JsonObjectRequest request;*/
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View vista = inflater.inflate(R.layout.fragment_pub_frglistadomisanuncios, container, false);
         pub_rclvpublicidad = vista.findViewById(R.id.pub_lmarclvmisanuncios);
         pub_listapublicidad = new ArrayList<>();
+
+//        requestQueue = Volley.newRequestQueue(getContext());
+
+//        parseJSON();
 
         //cargar la lista
         pub_cargarLista();
@@ -34,6 +53,42 @@ public class pub_frglistadomisanuncios extends Fragment {
 
         return vista;
     }
+
+    /*private void parseJSON() {
+        String URL = "";
+
+        request = new JsonObjectRequest(Request.Method.GET, URL, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        publicidad = null;
+                        JSONArray json=response.optJSONArray("consulta"); ///--------------------///--------------------///--------------------
+                        try {
+                            pub_listapublicidad.clear();
+                            for (int i = 0; i<json.length();i++){
+                                publicidad=new Publicidad();
+                                JSONObject jsonObject=null;
+                                jsonObject=json.getJSONObject(i);
+                                publicidad.setPub_strtitulo(jsonObject.optString("pubTitulo"));
+                                pub_listapublicidad.add(publicidad);
+
+                            }
+                            pub_adplista = new pub_adplistadofrglistado(getContext(), pub_listapublicidad);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getContext(), "Error al consultar"+ error.toString(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        requestQueue.add(request);
+
+    }*/
 
     public void pub_cargarLista() {
         pub_listapublicidad.add(new Publicidad("Publicidad 1","18/09/2022","30","100","59%",R.drawable.ic_launcher_background));
