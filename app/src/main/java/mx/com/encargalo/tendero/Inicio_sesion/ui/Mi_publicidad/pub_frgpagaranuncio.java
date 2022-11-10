2d63e9cc790ad840303775c2b0d957c23e7224bc
73769972@continental.edu.pe <73769972@continental.edu.pe>
2022-11-10 15:20:33 -0500

    Nuevo

--- app/src/main/java/mx/com/encargalo/tendero/Inicio_sesion/ui/Mi_publicidad/pub_frgpagaranuncio.java
+++ app/src/main/java/mx/com/encargalo/tendero/Inicio_sesion/ui/Mi_publicidad/pub_frgpagaranuncio.java
@@ -1,32 +1,87 @@
 package mx.com.encargalo.tendero.Inicio_sesion.ui.Mi_publicidad;
 
-import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.Navigation;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.Date;
+import java.util.Hashtable;
+import java.util.Map;
+
 import mx.com.encargalo.R;
+import mx.com.encargalo.tendero.UTIL.DATOS;
+
+import static android.content.Context.MODE_PRIVATE;
 
 public class pub_frgpagaranuncio extends Fragment {
+
     Button pub_pabtnpagoefectivo;
     Button pub_pabtncancelarpago;
     Button pub_pabtnpagaranuncio;
+    View auxview;
+    TextView pub_patxtcargodia, pub_patxtdia, pub_patxttotalpago,
+            pub_patxtfechadesde, pub_patxtfechahasta;
+    float preciot = 0;
+    RequestQueue request;
+    JsonObjectRequest jsonObjectRequest;
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View vista = inflater.inflate(R.layout.fragment_pub_frgpagaranuncio, container, false);
+
+        request= Volley.newRequestQueue(getContext());
+
         pub_pabtnpagoefectivo=vista.findViewById(R.id.pub_pabtnpagoefectivo);
         pub_pabtncancelarpago=vista.findViewById(R.id.pub_pabtncancelarpago);
         pub_pabtnpagaranuncio=vista.findViewById(R.id.pub_pabtnpagaranuncio);
+
+        pub_patxtcargodia=vista.findViewById(R.id.pub_patxtcargodia);
+        pub_patxtdia=vista.findViewById(R.id.pub_patxtdia);
+        pub_patxttotalpago=vista.findViewById(R.id.pub_patxttotalpago);
+        pub_patxtfechadesde=vista.findViewById(R.id.pub_patxtfechadesde);
+        pub_patxtfechahasta=vista.findViewById(R.id.pub_patxtfechahasta);
+
+        SharedPreferences sharedPreferences =
+                getContext().getSharedPreferences(DATOS.SHAREDPREFERENCES, MODE_PRIVATE);
+
+        Date fecha_inicio = new Date(Date.parse(sharedPreferences.getString("fechaInicio",""))) ;
+        Date fecha_fin = new Date(Date.parse(sharedPreferences.getString("fechaFin",""))) ;
+        Date res = new Date(fecha_fin.getTime() - fecha_inicio.getTime());
+
+        float preciodia = 1;
+        int dias = (int) (res.getTime()/86400000);
+        preciot = preciodia*dias;
+        pub_patxtfechadesde.setText(sharedPreferences.getString("fechaInicio",""));
+        pub_patxtfechahasta.setText(sharedPreferences.getString("fechaFin",""));
+        pub_patxtcargodia.setText(preciodia+" $");
+        pub_patxtdia.setText(dias+" dias");
+        pub_patxttotalpago.setText(preciot+" $");
+
         pub_pabtncancelarpago.setVisibility(View.INVISIBLE);
         pub_pabtnpagaranuncio.setVisibility(View.INVISIBLE);
+
         pub_pabtnpagoefectivo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -35,45 +90,72 @@ public class pub_frgpagaranuncio extends Fragment {
 
             }
         });
+
         pub_pabtnpagaranuncio.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                View pub_pavwauxview = view;
-                showDialogoPagar(pub_pavwauxview);
+                auxview = view;
+                showDialogoPagar();
             }
         });
+
         pub_pabtncancelarpago.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                View pub_pavwauxview = view;
-                showDialogoCancelar(pub_pavwauxview);
+                auxview = view;
+                showDialogoCancelar();
             }
         });
 
         return vista;
     }
 
-
-
-    /**
-     * Dialog !
-     */
-
-
-    private void  showDialogoPagar(View auxview){
+    private void  showDialogoPagar(){
         final Dialog dialog = new Dialog(getContext());
         dialog.setContentView(R.layout.pub_lytmodelpagaranuncio);
         dialog.show();
-        Button pub_pabtnaceptar = dialog.findViewById(R.id.pub_mpabtnaceptar);
-        Button pub_pabtncancelar = dialog.findViewById(R.id.pub_mpabtncancelar);
-        pub_pabtnaceptar.setOnClickListener(new View.OnClickListener() {
+        Button btnpagar = dialog.findViewById(R.id.pub_mpabtnaceptar);
+        Button btncancelar = dialog.findViewById(R.id.pub_mpabtncancelar);
+        btnpagar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Navigation.findNavController(auxview).navigate(R.id.nav_codigopago);
+                String myURL = DATOS.IP_SERVER+ "a_nuevo_anuncio.php";
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, myURL,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                Toast.makeText(getContext(), "Publicación registrada", Toast.LENGTH_LONG).show();
+                                Navigation.findNavController(auxview).navigate(R.id.nav_crearanuncio);
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getContext(), error.toString(), Toast.LENGTH_LONG).show();
+                        System.out.println(error.toString());
+                    }
+                }){
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        SharedPreferences sharedPreferences =
+                                getContext().getSharedPreferences(DATOS.SHAREDPREFERENCES, MODE_PRIVATE);
+                        Map<String, String> params = new Hashtable<String, String>();
+                        params.put("pathImg", sharedPreferences.getString("pathImg",""));
+                        params.put("titulo", sharedPreferences.getString("titulo",""));
+                        params.put("descripcion", sharedPreferences.getString("descripcion",""));
+                        params.put("linkRed", sharedPreferences.getString("linkRed",""));
+                        params.put("fechaInicio", sharedPreferences.getString("fechaInicio",""));
+                        params.put("fechaFin", sharedPreferences.getString("fechaFin",""));
+                        params.put("idTienda", sharedPreferences.getString(DATOS.VARGOB_ID_TENDERO,""));
+                        params.put("montoPubli", preciot+"" );
+                        params.put("fechaPago", sharedPreferences.getString("fechaInicio",""));
+                        return params;
+                    }
+                };
+                request.add(stringRequest);
                 dialog.dismiss();
             }
         });
-        pub_pabtncancelar.setOnClickListener(new View.OnClickListener() {
+        btncancelar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 dialog.dismiss();
@@ -81,20 +163,20 @@ public class pub_frgpagaranuncio extends Fragment {
         });
     }
 
-    private void  showDialogoCancelar(View auxview){
+    private void  showDialogoCancelar(){
         final Dialog dialog = new Dialog(getContext());
         dialog.setContentView(R.layout.pub_lytmodelcancelaranuncio);
         dialog.show();
-        Button pub_pabtnaceptar = dialog.findViewById(R.id.pub_mcabtnaceptar);
-        Button pub_pabtncancelar = dialog.findViewById(R.id.pub_mcabtncancelar);
-        pub_pabtnaceptar.setOnClickListener(new View.OnClickListener() {
+        Button btnpagar = dialog.findViewById(R.id.pub_mcabtnaceptar);
+        Button btncancelar = dialog.findViewById(R.id.pub_mcabtncancelar);
+        btnpagar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Navigation.findNavController(auxview).navigate(R.id.nav_crearanuncio);
+
                 dialog.dismiss();
             }
         });
-        pub_pabtncancelar.setOnClickListener(new View.OnClickListener() {
+        btncancelar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 dialog.dismiss();
